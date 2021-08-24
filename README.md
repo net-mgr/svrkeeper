5c4028f3abfaf499d6e5e077b4ff044ca1294401
shu1oka28 <40012404+shu1oka28@users.noreply.github.com>
2021-08-25 02:12:44 +0900

    add test_driver

--- README.md
+++ README.md
@@ -31,17 +31,47 @@ $ pip3 install -r requirements.txt
 
 ## テストの実行方法
 
-`./run.sh`を実行し、設定ファイルを作成したときに使用したパスワードを入力する。
+`./run.sh`を実行する。
+
 ```bash
 $ ./run.sh
+```
+
+テストはカテゴリを1つ指定することで、そのカテゴリのみ実行されます。
+
+```bash
+$ ./run.sh login
+$ ./run.sh mail
+$ ./run.sh web
+$ ./run.sh edit
+$ ./run.sh backup
+$ ./run.sh dns
+$ ./run.sh access
+```
+テストを実行するとパスワードの入力を求められます。設定ファイルを作成したときに使用したパスワードを入力してください。
+
+```bash
 # Input setting file password: 
 ##################################################
 # Results of each category
+# Category 1: Login test
+# => All tests were passed.
+# Category 2: Mail test
+# => All tests were passed.
 # Category 3: Web test
+# => All tests were passed.
+# Category 4: File editing test
+# => All tests were passed.
+# Category 5: Backup test
+# => All tests were passed.
+# Category 6: DNS test
+# => All tests were passed.
+# Category 7: Access point test
 # => All tests were passed.
 ##################################################
 # Total result
 # => All tests were passed.
+
 ```
 
 テスト結果は各カテゴリごとに出力されます。
@@ -54,19 +84,35 @@ $ ./run.sh
 
 また、テスト結果は以下のファイルに保存されています。
 
-|カテゴリ |保存先              |
-|:--------|:-------------------|
-|web      |results/Web_test.log|
+|カテゴリ           | 保存先                        |
+|:------------------|:------------------------------|
+| Login test        | results/login_test.log        |
+| Mail test         | results/mail_test.log         |
+| Web test          | results/Web_test.log          |
+| File editing test | results/edit_file_test.log    |
+| Backup test       | results/backup_test.log       |
+| DNS test          | results/dns_test.log          |
+| Access point test | results/access_point_test.log |
+
+
+
+
 
 ## ディレクトリ構成
 
 | ディレクトリ名 | 内容|
-| config |テストに必要なパラメータや暗号化した認証情報を保存|
+|:-----------|:----|
+| config |テストに必要なパラメータのファイルを保存。また、暗号化した認証情報は`config/setting.sec.sh`に暗号化して保存。（詳しくは[config/README.md](config/README.md)|
 | config_sample | configに設置するファイルの例を保存。テストには使わない。 |
 | results | テスト結果を保存 |
 | scripts | テストを実行するスクリプトを保存。すべて`run.sh`からのみ呼び出す|
 
 ## TODO
-- `scripts/web_test.sh`の内部からのアクセスのテスト
-- その他のテスト
-- `run.sh`でカテゴリを選んでテストする仕組み
+- `scripts/web_test.sh`の内部からのアクセスのテストの実装
+- その他のテストの実装（現在は`exit 0`のみになっている)
+    - `scripts/access_point_test.sh`
+    - `scripts/backup_test.sh`
+    - `scripts/dns_test.sh`
+    - `scripts/edit_file_test.sh`
+    - `scripts/login_test.sh`
+    - `scripts/mail_test.sh`
--- run.sh
+++ run.sh
@@ -2,6 +2,40 @@
 
 SCRIPTS_DIR=$(cd $(dirname $0); pwd)
 
+
+# テスト対象を選択
+login_test_select=1
+mail_test_select=1
+web_test_select=1
+edit_file_test_select=1
+backup_test_select=1
+dns_test_select=1
+access_point_test_select=1
+if [ $# -eq 1 ]; then
+	if [ "$1" != "login" ];then
+		login_test_select=0
+	fi
+	if [ "$1" != "mail" ];then
+		mail_test_select=0
+	fi
+	if [ "$1" != "web" ];then
+		web_test_select=0
+	fi
+	if [ "$1" != "edit" ];then
+		edit_file_test_select=0
+	fi
+	if [ "$1" != "backup" ];then
+		backup_test_select=0
+	fi
+	if [ "$1" != "dns" ];then
+		dns_test_select=0
+	fi
+	if [ "$1" != "access" ];then
+		access_point_test_select=0
+	fi
+fi
+	
+
 # Setting for driving tests
 setting_file_sec=${SCRIPTS_DIR}/config/setting.sec.sh
 setting_file=${SCRIPTS_DIR}/config/setting.sh
@@ -23,21 +57,69 @@ fi
 . $setting_file
 rm $setting_file
 
-# There is no need to decrypt setting.sec.sh again
-#  in mail_test.sh and web_test.sh
-export SETTING_FILE_INCLUDED=1
-
 #####################################################################################
 # テストの実行 (並列) 。バックグラウンドで実行し、waitで待つ
 
 mkdir -p $SCRIPTS_DIR/results/
-$SCRIPTS_DIR/scripts/web_test.sh >$SCRIPTS_DIR/results/web_test.log 2>&1   &
-WEB_TEST_PID=$!
+
+if [ $login_test_select -eq 1 ];then
+	$SCRIPTS_DIR/scripts/login_test.sh >$SCRIPTS_DIR/results/login_test.log 2>&1   &
+	LOGIN_TEST_PID=$!
+fi
+if [ $mail_test_select -eq 1 ];then
+	$SCRIPTS_DIR/scripts/mail_test.sh >$SCRIPTS_DIR/results/mail_test.log 2>&1   &
+	MAIL_TEST_PID=$!
+fi
+if [ $web_test_select -eq 1 ];then
+	$SCRIPTS_DIR/scripts/web_test.sh >$SCRIPTS_DIR/results/web_test.log 2>&1   &
+	WEB_TEST_PID=$!
+fi
+if [ $edit_file_test_select -eq 1 ];then
+	$SCRIPTS_DIR/scripts/edit_file_test.sh >$SCRIPTS_DIR/results/edit_file_test.log 2>&1   &
+	EDIT_TEST_PID=$!
+fi
+if [ $backup_test_select -eq 1 ];then
+	$SCRIPTS_DIR/scripts/backup_test.sh >$SCRIPTS_DIR/results/backup_test.log 2>&1   &
+	BACKUP_TEST_PID=$!
+fi
+if [ $dns_test_select -eq 1 ];then
+	$SCRIPTS_DIR/scripts/dns_test.sh >$SCRIPTS_DIR/results/dns_test.log 2>&1   &
+	DNS_TEST_PID=$!
+fi
+if [ $access_point_test_select -eq 1 ];then
+	$SCRIPTS_DIR/scripts/access_point_test.sh >$SCRIPTS_DIR/results/access_point_test.log 2>&1   &
+	ACCESS_TEST_PID=$!
+fi
 
 
-wait $WEB_TEST_PID
-web_test_passed=$?
-
+if [ $login_test_select -eq 1 ];then
+	wait $LOGIN_TEST_PID
+	login_test_passed=$?
+fi
+if [ $mail_test_select -eq 1 ];then
+	wait $MAIL_TEST_PID
+	mail_test_passed=$?
+fi
+if [ $web_test_select -eq 1 ];then
+	wait $WEB_TEST_PID
+	web_test_passed=$?
+fi
+if [ $edit_file_test_select -eq 1 ];then
+	wait $EDIT_TEST_PID
+	edit_file_test_passed=$?
+fi
+if [ $backup_test_select -eq 1 ];then
+	wait $BACKUP_TEST_PID
+	backup_test_passed=$?
+fi
+if [ $dns_test_select -eq 1 ];then
+	wait $DNS_TEST_PID
+	dns_test_passed=$?
+fi
+if [ $access_point_test_select -eq 1 ];then
+	wait $ACCESS_TEST_PID
+	access_point_test_passed=$?
+fi
 #####################################################################################
 # テスト結果の出力
 
@@ -61,13 +143,27 @@ print_result(){
 echo "##################################################"
 echo "# Results of each category"
 
-#print_result "# Category 1: Login test"        $login_test_passed
-#print_result "# Category 2: Mail test"         $mail_test_passed
-print_result "# Category 3: Web test"          $web_test_passed
-#print_result "# Category 4: File editing test" $edit_file_test_passed
-#print_result "# Category 5: Backup test"       $backup_test_passed
-#print_result "# Category 6: DNS test"          $dns_test_passed
-#print_result "# Category 7: Access point test" $access_point_test_passed
+if [ $login_test_select -eq 1 ];then
+	print_result "# Category 1: Login test"        $login_test_passed
+fi
+if [ $mail_test_select -eq 1 ];then
+	print_result "# Category 2: Mail test"         $mail_test_passed
+fi
+if [ $web_test_select -eq 1 ];then
+	print_result "# Category 3: Web test"          $web_test_passed
+fi
+if [ $edit_file_test_select -eq 1 ];then
+	print_result "# Category 4: File editing test" $edit_file_test_passed
+fi
+if [ $backup_test_select -eq 1 ];then
+	print_result "# Category 5: Backup test"       $backup_test_passed
+fi
+if [ $dns_test_select -eq 1 ];then
+	print_result "# Category 6: DNS test"          $dns_test_passed
+fi
+if [ $access_point_test_select -eq 1 ];then
+	print_result "# Category 7: Access point test" $access_point_test_passed
+fi
 
 echo "##################################################"
 print_result "# Total result" $all_tests_passed
